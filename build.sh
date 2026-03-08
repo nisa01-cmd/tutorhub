d8cc324330d492a64a6ecd950e11cd964d613462
nisa01-cmd <gondanenisarga15@gmail.com>
2026-03-08 14:19:49 +0530

    create admin automatically on deploy

--- build.sh
+++ build.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
 pip install -r requirements.txt
-python manage.py collectstatic --noinput
+
 python manage.py migrate
+
+python manage.py collectstatic --noinput
+
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@example.com", "admin123")
+END
